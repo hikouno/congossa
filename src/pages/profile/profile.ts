9d569fbe5cd58b5a54f4d015e07ba3dfcc8ba8d5
AlexisDuSale <alexis.maffart@gmail.com>
2018-01-24 15:38:54 +0100

    profile again

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -13,7 +13,9 @@ export class ProfilePage {
 	phone: string = "0678954512";
 	formation : string = "PSMSC Master - Performance in Software, Media and Scientific";
 	skills : string = "Organisé, Cultivé, Interessant, Handsome"
-	description : string = "Looking for an internship in computer science.";
+	description : string = "Looking for an internship in computer science. Quanta autem vis amicitiae sit, ex hoc intellegi \
+	maxime potest, quod ex infinita societate generis humani, quam conciliavit ipsa natura, ita contracta res est et adducta \
+	in angustum ut omnis caritas aut inter duos aut inter paucos iungeretur.";
 	
   constructor(public navCtrl: NavController) {
     //this.firstname = navParams.get("firstname");
