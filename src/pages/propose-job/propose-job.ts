0f1501b993c62c06a2af6d1cd2a689fa36973fb7
AlexisDuSale <alexis.maffart@gmail.com>
2018-02-05 16:32:22 +0100

    second lil clear

--- src/pages/propose-job/propose-job.ts
+++ src/pages/propose-job/propose-job.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+//pages
+import { ProfilePage } from '../profile/profile';
+import { ListeConversationsPage } from '../listeConversations/listeConversations';
+
 /**
  * Generated class for the ProposeJobPage page.
  *
