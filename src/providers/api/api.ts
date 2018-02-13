30ee3a768a98aefc929fa4e84441e99f906d707d
AlexisDuSale <alexis.maffart@gmail.com>
2018-02-13 15:13:42 +0100

    nothing

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -7,11 +7,21 @@ import { Injectable } from '@angular/core';
   See https://angular.io/guide/dependency-injection for more info on providers
   and Angular DI.
 */
+
+
+
+/*
+This provider is used to separate the requests to the server from the main code.
+*/
+
 @Injectable()
 export class ApiProvider {
 
   constructor(public http: HttpClient) {
     console.log('Hello ApiProvider Provider');
   }
+
+
+
 
 }
